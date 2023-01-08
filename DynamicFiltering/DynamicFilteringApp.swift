a71406068db5b31fa1fc7a2048dde99059512e46
CodeByDzyne <wdenley@dzynesolutions.com>
2023-01-08 22:07:45 +1100

    Initial Commit

---
+++ DynamicFiltering/DynamicFilteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFilteringApp.swift
+//  DynamicFiltering
+//
+//  Created by Wealthy Waz on 8/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFilteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
